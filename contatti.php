5ca685898f1b954052ead421555db26858428a46
DavideD <113457526+Informatica-d@users.noreply.github.com>
2025-05-19 23:08:52 +0200

    Add files via upload

---
+++ contatti.php
@@ -0,0 +1,16 @@
+<?php include "header.php"; ?>
+<title>Contatti</title>
+<main>
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9731.393092584101!2d12.645646331678801!3d41.72188845138864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1325853780da4035%3A0xf1ead19a6715dc0c!2sScuola%20Padri%20Somaschi!5e0!3m2!1sit!2sit!4v1747408624543!5m2!1sit!2sit" 
+            width="370" 
+            height="250" 
+            style="border:0px;" 
+            allowfullscreen="" 
+            loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+
+
+
+</main>
